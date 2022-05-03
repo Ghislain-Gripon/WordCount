3a78462a7e5e26676554c26750fd8706ae70c1f1
Ghislain-Gripon <ghislain.gripon@protonmail.com>
2022-05-03 21:07:00 +0200

    Configuration class added with config file, needs to introduce support with test coverage (scalatest & cucumber) plus change hardcoded values for config class calls

--- src/main/scala/WordCountCucumberScala_GhislainGripon/Configuration.scala
+++ src/main/scala/WordCountCucumberScala_GhislainGripon/Configuration.scala
@@ -3,20 +3,31 @@ package WordCountCucumberScala_GhislainGripon
 import io.circe.Json
 import io.circe.yaml.parser
 import io.circe._
-import io.circe.parser._
 import cats.syntax.either._
 import scala.io.Source
+import scala.tools.nsc.io._
 
-class Configuration(configFilePath: String) {
-  var yamlString: Option[String] = None
+//If no path to a configuration file is provided, default location is taped, src/data/Config.yaml
+class Configuration(var configFilePath: String = "") {
+  private var _yamlString: Option[String] = None
+
+  if(configFilePath.isEmpty) {
+    configFilePath = (Path(new JFile(".").getCanonicalPath).parents(2).toString()
+      + "/data/Config.yaml")
+  }
+
   Control.using(Source.fromFile(configFilePath)) { source =>
-    yamlString = Some(source.mkString)
+    _yamlString = Some(source.mkString)
   }
 
-  val ConfigJson: Json = parser.parse(yamlString.asInstanceOf[String]).getOrElse(Json.Null)
+  private val _ConfigJson: Json = parser.parse(_yamlString.asInstanceOf[String]).getOrElse(Json.Null)
+  private val _cursor: HCursor = _ConfigJson.hcursor
+  private val _directory: String = _cursor.downField("data").downField("directory").as[String].toString
 
-  def getField(field_path: String): Any = {
-    ConfigJson
-  }
+  val version: String = _cursor.downField("version").as[String].toString
+  val test_directory: String = _directory + "/" + _cursor.downField("data").downField("test").as[String].toString
+  val main_directory: String = _directory + "/" + _cursor.downField("data").downField("main").as[String].toString
+  val thread_use_or_not: Boolean = _cursor.downField("threads").downField("used_or_not").as[Boolean].getOrElse[Boolean]
+  val max_thread_num: Int = _cursor.downField("threads").downField("maximum_number").as[Int].asInstanceOf[Int]
 
 }
